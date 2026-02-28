996ec04d631f37ff1c7e9d7b8a740d353e7645e1
Ilija Spasic <spasicilija1@gmail.com>
2026-02-28 22:20:17 +0100

    feature: add initial migrations

---
+++ src/AccommodationService.Infrastructure/Migrations/20260228200839_InitModels.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccommodationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accommodations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Location = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Amenities = table.Column<List<string>>(type: "jsonb", nullable: false),
+                    Pictures = table.Column<List<string>>(type: "jsonb", nullable: false),
+                    MinGuests = table.Column<int>(type: "integer", nullable: false),
+                    MaxGuests = table.Column<int>(type: "integer", nullable: false),
+                    HostId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AutoApproval = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accommodations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accommodations_HostId",
+                table: "Accommodations",
+                column: "HostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accommodations_Location",
+                table: "Accommodations",
+                column: "Location");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accommodations");
+        }
+    }
+}
